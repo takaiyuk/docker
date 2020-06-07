364075d2a913795f6051310348bbf3b0c673d810
takaiyuk <takaishikawa1225@gmail.com>
2020-06-07 21:45:40 +0900

    Fix

--- gpu/table/run.sh
+++ gpu/table/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run --runtime=nvidia -d --name $1 -p 8888:8888 -v ${PWD}:/tf/notebooks -v ${HOME}/.kaggle:/root/.kaggle/ takaiyuk/ml-table-gpu
+sudo docker run --runtime=nvidia -d --name $1 -p 8888:8888 -v ${PWD}:/workspace -v ${HOME}/.kaggle:/root/.kaggle/ takaiyuk/ml-table-gpu
