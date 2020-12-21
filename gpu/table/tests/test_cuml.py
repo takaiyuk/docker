f9b9cff327a4ea6a95db22ce9cd99ddc3c33f1d1
takaiyuk <takaishikawa1225@gmail.com>
2020-12-21 23:36:07 +0900

    Fix tests

--- gpu/table/tests/test_cuml.py
+++ gpu/table/tests/test_cuml.py
@@ -1,14 +1,16 @@
 import cudf
 from cuml.cluster import DBSCAN
 
-# Create and populate a GPU DataFrame
-gdf_float = cudf.DataFrame()
-gdf_float['0'] = [1.0, 2.0, 5.0]
-gdf_float['1'] = [4.0, 2.0, 1.0]
-gdf_float['2'] = [4.0, 2.0, 1.0]
 
-# Setup and fit clusters
-dbscan_float = DBSCAN(eps=1.0, min_samples=1)
-dbscan_float.fit(gdf_float)
+def test_cuml_fit_clusters():
+    # Create and populate a GPU DataFrame
+    gdf_float = cudf.DataFrame()
+    gdf_float['0'] = [1.0, 2.0, 5.0]
+    gdf_float['1'] = [4.0, 2.0, 1.0]
+    gdf_float['2'] = [4.0, 2.0, 1.0]
 
-print(dbscan_float.labels_)
+    # Setup and fit clusters
+    dbscan_float = DBSCAN(eps=1.0, min_samples=1)
+    dbscan_float.fit(gdf_float)
+
+    print(dbscan_float.labels_)
--- gpu/table/tests/test_tensorflow.py
+++ gpu/table/tests/test_tensorflow.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 
-num_gpu_available = len(tf.config.experimental.list_physical_devices("GPU"))
-print("Num GPUs Available:", num_gpu_available)
-assert num_gpu_available == 1
+
+def test_num_gpu():
+    num_gpu_available = len(tf.config.experimental.list_physical_devices("GPU"))
+    print("Num GPUs Available:", num_gpu_available)
+    assert num_gpu_available == 1
