a85b63150870149ba68ebb07fc734093178397a9
takaiyuk <takaishikawa1225@gmail.com>
2020-12-21 13:42:15 +0900

    Fix test_tensorflow

--- gpu/table/tests/test_tensorflow.py
+++ gpu/table/tests/test_tensorflow.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
 
-print("Num GPUs Available:", len(tf.config.experimental.list_physical_devices("GPU")))
-
-assert len(tf.config.experimental.list_physical_devices("GPU")) == 1
+num_gpu_available = len(tf.config.experimental.list_physical_devices("GPU"))
+print("Num GPUs Available:", num_gpu_available)
+assert num_gpu_available == 1
