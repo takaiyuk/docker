e4856bca527ae54e90576d50dc76c5b777215e5c
takaiyuk <takaishikawa1225@gmail.com>
2021-05-24 08:50:47 +0900

    Update python3.8 and use poetry

--- gpu/table/test.sh
+++ gpu/table/test.sh
@@ -2,21 +2,16 @@
 docker_run () {
     docker run --runtime=nvidia -it -d --name test_ml-table-gpu -v ${PWD}:/workspace takaiyuk/ml-table-gpu
 }
-docker_exec () {
-    docker exec -it test_ml-table-gpu /bin/bash
+
+docker_exec_test () {
+    docker exec test_ml-table-gpu /bin/bash -c ". /venv/bin/activate && python tests/test_cudf.py; python tests/test_lightgbm.py; python tests/test_tensorflow.py"
 }
-run_test () {
-    . /venv/bin/activate
-    python tests/test_cudf.py
-    python tests/test_lightgbm.py
-    python tests/test_tensorflow.py
-}
+
 docker_kill () {
     docker stop test_ml-table-gpu
     docker rm test_ml-table-gpu
 }
 
 docker_run
-docker_exec
-run_test
+docker_exec_test
 docker_kill
